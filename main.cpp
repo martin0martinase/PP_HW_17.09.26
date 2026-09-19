06caa6d38d4b68c079389a29705895910cddf06c
Chernikov Leonid <leonidcernikov43@gmail.com>
2026-09-19 21:30:32 +0300

    Data passing solution

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <thread>
+
+struct Data{
+    double r;
+    size_t tests;
+    size_t result;
+};// я хз как иначе это сделать но вот это в потоковую ф.
+
 int main(){
     return 0;
 }
